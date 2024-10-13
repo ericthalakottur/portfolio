13219169fb38ce768f8621ddb0208eacdf85d561
Eric Thalakottur <184715709+ericthalakottur@users.noreply.github.com>
2024-10-13 17:51:50 +0530

    Add HomeComponent and PageNotFoundComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { Social } from './social';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  socials: Social[] = [
+    {"name": "AtCoder", "url": "https://atcoder.jp/users/erict"},
+    {"name": "GitHub", "url": "https://github.com/ericthalakottur"},
+    {"name": "HackTheBox", "url": "https://app.hackthebox.com/profile/904476"},
+    {"name": "LinkedIn", "url": "https://www.linkedin.com/in/ericthalakottur"},
+  ];
+}
