f87b7af3b80e826aa8564416a8720d5dfedf4509
Eric Thalakottur <184715709+ericthalakottur@users.noreply.github.com>
2025-03-29 19:40:58 +0530

    Add navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HomeComponent],
+    imports: [RouterOutlet, NavigationBarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
